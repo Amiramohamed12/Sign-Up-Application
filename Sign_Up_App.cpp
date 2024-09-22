165e3b7586e6e379ce90f8ae8c68168de82ee38d
Amira <161435300+Amiramohamed12@users.noreply.github.com>
2024-09-22 18:26:33 +0300

    Update Sign_Up_App.cpp

--- Sign_Up_App.cpp
+++ Sign_Up_App.cpp
@@ -3,6 +3,7 @@
 #include <string>
  
 int num_users=0;
+constexpr int max_users=100;
 class signUp
 {
 public:
@@ -17,16 +18,13 @@ std::array<signUp,100> users;
 void signUp :: AddRecord ( const std::string name,const int age){
      users[num_users].user_name=name;
      users[num_users].user_age=age;
-     std::cout<<"User ID: "<<num_users+1<<"\n";
-     std::cout<<"\n";
-     std::cout<<"User record added successfully. \n";
-     std::cout<<"\n";
+     std::cout<<"User ID: "<<num_users+1<<"\n \n";
+     std::cout<<"User record added successfully. \n \n";
       }
 
 void signUp :: FetchRecord (const int user_ID){
     std::cout<<"User Name:"<<users[user_ID-1].user_name<<"\n";
-    std::cout<<"User Age: "<<users[user_ID-1].user_age<<"\n";
-    std::cout<<"\n";
+    std::cout<<"User Age: "<<users[user_ID-1].user_age<<"\n \n";
       }
 
 enum class option{
@@ -49,8 +47,7 @@ void show_options(){
 std::cout<<"Please select an option: \n";
 std::cout<<"1: Add Record \n";
 std::cout<<"2: Fetch Record \n";
-std::cout<<"3: Quit \n";
-std::cout<<"\n";
+std::cout<<"3: Quit \n \n";
 }
 
 int main(){
@@ -61,10 +58,9 @@ int age=0;
 std::string name;
 int user_ID=0;
 
-std::cout<<"User signUp Application \n";
-std::cout<<"\n";
+std::cout<<"User signUp Application \n \n";
 
-while (isRunning&&num_users<=99)
+while (isRunning)
 { show_options();
 std::cout<<"Enter option: ";
 std::cin>>option;
@@ -72,6 +68,9 @@ std::cout<<"\n";
 switch (option)
 {
 case option::add_record:
+    if(num_users==max_users){
+     std::cout<<"Invalid record. Max numbers of records = 100 \n \n";   
+     break; }   
     std::cout<<"Add User. Please enter user name and age: \n";
     std::cout<<"Name: ";
     std::cin>>name;
@@ -81,23 +80,23 @@ case option::add_record:
     num_users++;
     break;
 
-case option::fetch_record:
-       std::cout<<"Please enter user ID: \n";
-       std::cout<<"User ID: ";
-       std::cin>>user_ID;
-       users[user_ID].FetchRecord(user_ID);
-    break;
+    case option::fetch_record:
+        std::cout<<"Please enter user ID: \n";
+        std::cout<<"User ID: ";
+        std::cin>>user_ID;
+        if(user_ID<0 || user_ID>num_users){
+            std::cout<<"Invalid User ID \n \n";
+            break ;
+            }
+        users[user_ID].FetchRecord(user_ID);
+        break;
 
-case option::quit:
-    isRunning=false;
-    break;
+    case option::quit:
+        isRunning=false;
+        break;
 
-default:
-   std::cout<<"No vaild option  \n";
-    break;
-} }
-
-if(num_users==100)
-std::cout<<"Invalid record. Max numbers of records = 100";
-
-} 
+    default:
+    std::cout<<"No vaild option  \n";
+        break;
+    } } 
+    }
